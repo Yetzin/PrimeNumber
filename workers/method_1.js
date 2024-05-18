5cb2b1a43ee52ad2277ee3828932de4ea165fc27
Yetzín <mermin@protonmail.ch>
2024-05-17 19:30:24 -0600

    Deleting binary files

--- workers/method_1.js
+++ workers/method_1.js
@@ -4,21 +4,21 @@ function method(i) {
     const start = new Date();
     let numeros = [ 2 ], latest = 3;
     while(i > numeros.length) {
-		let div = false, k = 0;
+        let div = false, k = 0;
         while(k < numeros.length) {
-			div = latest % numeros[k] == 0;
-			if(div) {
-				latest++;
-				k = 0;
+            div = latest % numeros[k] == 0;
+            if(div) {
+                latest++;
+                k = 0;
                 break;
-			}
+            }
             k++;
-		}
-		if(!div) {
-			numeros.push(latest);
+        }
+        if(!div) {
+            numeros.push(latest);
             latest++;
-		}
-	}
+        }
+    }
     const end = new Date();
 
     return (`1) El número primo en la posición ${i} es: ${numeros[i-1]}\n${(end.getTime() - start.getTime())} ms`);
